ddcbad188b73c43aae2d60ddc63476f272e479ab
yueliyangzi <yueliyang@163.com>
2021-07-29 00:51:21 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::sync::RwLock;
 use lazy_static::lazy_static;
 use std::thread::sleep;
+use std::env;
 pub struct FileReader{
     pub path : &'static str,
 
@@ -24,36 +25,50 @@ impl FileReader {
 }
 
 fn main() {
-    let thread1 = thread::spawn(|| {
-        let f = FileReader::read("./sample.json");
-        let values:serde_json::Value = serde_json::from_reader(f).unwrap();
-        println!("整个字符串 t1：{:?}",values);
-        println!("name t1:{}",values["name"]);
-        println!("age t1:{}",values["age"]);
-        println!("hobby-sports t1:{}",values["hobby"]["sports"]);
-        println!("hobby-food t1:{}",values["hobby"]["food"]);
-        println!("t1 {:?}", values["name"].as_str().unwrap());
-        println!("t1 {:?}", values["age"].as_i64().unwrap());
-    });
-    let thread2 = thread::spawn(|| {
-        FileReader::write()
-    });
-    let thread3 = thread::spawn(|| {
-        let f = FileReader::read("./sample.json");
-        let values:serde_json::Value = serde_json::from_reader(f).unwrap();
-        println!("整个字符串 t3：{:?}",values);
-        println!("name t3:{}",values["name"]);
-        println!("age t3:{}",values["age"]);
-        println!("hobby-sports t3:{}",values["hobby"]["sports"]);
-        println!("hobby-food t3:{}",values["hobby"]["food"]);
-        println!("t3 {:?}", values["name"].as_str().unwrap());
-        println!("t3 {:?}", values["age"].as_i64().unwrap());
-    });
-
-    thread1.join().unwrap();
-    thread2.join().unwrap();
-    thread3.join().unwrap();
+    // let thread1 = thread::spawn(|| {
+    //
+    //     let f = FileReader::read("./sample.json");
+    //     let values:serde_json::Value = serde_json::from_reader(f).unwrap();
+    //     println!("整个字符串 t1：{:?}",values);
+    //     println!("name t1:{}",values["name"]);
+    //     println!("age t1:{}",values["age"]);
+    //     println!("hobby-sports t1:{}",values["hobby"]["sports"]);
+    //     println!("hobby-food t1:{}",values["hobby"]["food"]);
+    //     println!("t1 {:?}", values["name"].as_str().unwrap());
+    //     println!("t1 {:?}", values["age"].as_i64().unwrap());
+    // });
+    // let thread2 = thread::spawn(|| {
+    //     FileReader::write()
+    // });
+    // let thread3 = thread::spawn(|| {
+    //     let f = FileReader::read("./sample.json");
+    //     let values:serde_json::Value = serde_json::from_reader(f).unwrap();
+    //     println!("整个字符串 t3：{:?}",values);
+    //     println!("name t3:{}",values["name"]);
+    //     println!("age t3:{}",values["age"]);
+    //     println!("hobby-sports t3:{}",values["hobby"]["sports"]);
+    //     println!("hobby-food t3:{}",values["hobby"]["food"]);
+    //     println!("t3 {:?}", values["name"].as_str().unwrap());
+    //     println!("t3 {:?}", values["age"].as_i64().unwrap());
+    // });
+    //
+    // thread1.join().unwrap();
+    // thread2.join().unwrap();
+    // thread3.join().unwrap();
 
     //let r = INC.read();
-
+    let args: Vec<String> = env::args().collect();
+    //println!("get command args :{:?} ", args);
+    //println!("get env args : ");
+    for (key, value) in env::vars() {
+        //println!("  {}  =>  {}", key, value);
+    }
+    let key = "PATH";
+    match env::var(key) {
+        Ok(val) => {
+            // val is String, splited by ";"
+            println!("val =>{}",val);
+        },
+        Err(e) => println!("couldn't interpret {}: {}", key, e),
+    }
 }
