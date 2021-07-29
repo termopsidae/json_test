7e224b311ec0845c3a1f2bae7cef6bc3d6f4bb02
yueliyangzi <yueliyang@163.com>
2021-07-29 12:07:27 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -25,38 +25,45 @@ impl FileReader {
 }
 
 fn main() {
-    // let thread1 = thread::spawn(|| {
-    //
-    //     let f = FileReader::read("./sample.json");
-    //     let values:serde_json::Value = serde_json::from_reader(f).unwrap();
-    //     println!("整个字符串 t1：{:?}",values);
-    //     println!("name t1:{}",values["name"]);
-    //     println!("age t1:{}",values["age"]);
-    //     println!("hobby-sports t1:{}",values["hobby"]["sports"]);
-    //     println!("hobby-food t1:{}",values["hobby"]["food"]);
-    //     println!("t1 {:?}", values["name"].as_str().unwrap());
-    //     println!("t1 {:?}", values["age"].as_i64().unwrap());
-    // });
-    // let thread2 = thread::spawn(|| {
-    //     FileReader::write()
-    // });
-    // let thread3 = thread::spawn(|| {
-    //     let f = FileReader::read("./sample.json");
-    //     let values:serde_json::Value = serde_json::from_reader(f).unwrap();
-    //     println!("整个字符串 t3：{:?}",values);
-    //     println!("name t3:{}",values["name"]);
-    //     println!("age t3:{}",values["age"]);
-    //     println!("hobby-sports t3:{}",values["hobby"]["sports"]);
-    //     println!("hobby-food t3:{}",values["hobby"]["food"]);
-    //     println!("t3 {:?}", values["name"].as_str().unwrap());
-    //     println!("t3 {:?}", values["age"].as_i64().unwrap());
-    // });
-    //
-    // thread1.join().unwrap();
-    // thread2.join().unwrap();
-    // thread3.join().unwrap();
+   env_t();
+//   json_t()
 
-    //let r = INC.read();
+}
+pub fn json_t(){
+    let thread1 = thread::spawn(|| {
+
+        let f = FileReader::read("./sample.json");
+        let values:serde_json::Value = serde_json::from_reader(f).unwrap();
+        println!("整个字符串 t1：{:?}",values);
+        println!("name t1:{}",values["name"]);
+        println!("age t1:{}",values["age"]);
+        println!("hobby-sports t1:{}",values["hobby"]["sports"]);
+        println!("hobby-food t1:{}",values["hobby"]["food"]);
+        println!("t1 {:?}", values["name"].as_str().unwrap());
+        println!("t1 {:?}", values["age"].as_i64().unwrap());
+    });
+    let thread2 = thread::spawn(|| {
+        FileReader::write()
+    });
+    let thread3 = thread::spawn(|| {
+        let f = FileReader::read("./sample.json");
+        let values:serde_json::Value = serde_json::from_reader(f).unwrap();
+        println!("整个字符串 t3：{:?}",values);
+        println!("name t3:{}",values["name"]);
+        println!("age t3:{}",values["age"]);
+        println!("hobby-sports t3:{}",values["hobby"]["sports"]);
+        println!("hobby-food t3:{}",values["hobby"]["food"]);
+        println!("t3 {:?}", values["name"].as_str().unwrap());
+        println!("t3 {:?}", values["age"].as_i64().unwrap());
+    });
+
+    thread1.join().unwrap();
+    thread2.join().unwrap();
+    thread3.join().unwrap();
+
+    let r = INC.read();
+}
+pub fn env_t(){
     let args: Vec<String> = env::args().collect();
     //println!("get command args :{:?} ", args);
     //println!("get env args : ");
